8c176a098813c8abee13cef12b800e2c1de1b550
machadof90 <machadofilho.ti@gmail.com>
2025-01-24 10:37:32 -0300

    Adiciona funções para desenhar padrões na matriz de LEDs

--- Tarefa_microcontrolador_GPIO_2.c
+++ Tarefa_microcontrolador_GPIO_2.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/pio.h"
 #include "hardware/clocks.h"
@@ -27,19 +28,88 @@ void inicializacao_maquina_pio(uint pino);
 void atribuir_cor_ao_led(const uint indice, const uint8_t r, const uint8_t g, const uint8_t b);
 void limpar_o_buffer();
 void escrever_no_buffer();
+void desenho(char letra);
+
+// ------MATRIZ-----
+
+int tamanho_matriz = 5;
+
+uint matrizint[5][5] = {
+    {24, 23, 22, 21, 20},
+		{15, 16, 17, 18, 19},
+		{14, 13, 12, 11, 10},
+		{5, 6, 7, 8, 9},
+		{4, 3, 2, 1, 0}
+};
+
+ char matriz_B[5][5]={
+  {'*','B','B','*','*'},
+  {'*','B','*','B','*'},
+  {'*','B','B','*','*'},
+  {'*','B','*','B','*'},
+  {'*','B','B','*','*'}
+ };
+
+char matriz_A[5][5]={
+  {'*','*','R','*','*'},
+  {'*','R','*','R','*'},
+  {'*','R','R','R','*'},
+  {'*','R','*','R','*'},
+  {'*','R','*','R','*'}
+};  
+char matriz_C[5][5]={
+  {'*','*','G','*','*'},
+  {'*','G','*','G','*'},
+  {'*','G','*','*','*'},
+  {'*','G','*','G','*'},
+  {'*','*','G','*','*'}
+};  
+char matriz_MARIO[5][5]={
+  {'R','R','R','R','R'},
+  {'R','P','P','P','P'},
+  {'R','B','R','R','B'},
+  {'B','B','B','B','B'},
+  {'Y','Y','*','Y','Y'}
+};  
+
+char arcoiris[5][5]={
+	{'R','R','Y','Y','Y'},
+  {'P','R','R','Y','G'},
+  {'P','P','W','G','G'},
+  {'P','B','C','C','G'},
+  {'B','B','B','C','C'}
+};
+char mosaico[5][5]={
+{ '*', '*', 'R', '*', '*' },
+{ '*', 'R', '*', 'B', '*' },
+{ '*', 'G', '*', 'Y', '*' },
+{ '*', 'B', '*', 'G', '*' },
+{ '*', '*', 'C', '*', '*' }
+};
+
 
 //-----FUNÇÃO PRINCIPAL-----
 int main(void){
 	// Inicializa matriz de LEDs NeoPixel.
 	inicializacao_maquina_pio(PINO_MATRIZ_LED);
+	sleep_ms(5000);
 	limpar_o_buffer();
+	desenho('#');
+	sleep_ms(3000);
+	desenho('A');
+	sleep_ms(3000);
+	desenho('B');
 
+	
+	
+		
 	escrever_no_buffer(); // Escreve os dados nos LEDs.
 
 	// A mágica acontece aqui :)
 	while (true){
-	
+		
 	}
+	return 0;
 }
 
 //-----FUNÇÕES COMPLEMENTARES-----
@@ -76,9 +146,66 @@ void atribuir_cor_ao_led(const uint indice, const uint8_t r, const uint8_t g, co
 }
 
 // Limpa o buffer de pixels.
-void limpar_o_buffer(){
-	for (uint i = 0; i < CONTADOR_LED; ++i)
-		atribuir_cor_ao_led(i,0,0,0);
+void limpar_o_buffer(){	
+	for (uint i = 0; i < CONTADOR_LED; ++i){
+	atribuir_cor_ao_led(i,0,0,0);
+	}			
+}
+
+void  desenho(char letra){
+	char (*matriz)[5];
+	if (letra == 'A') {
+        matriz = matriz_A;
+    } 
+		if (letra == 'B') {
+        matriz = matriz_B;
+    }
+		if (letra == 'C') {
+        matriz = matriz_C;
+    }
+		if ( letra == 'M'){
+			matriz = matriz_MARIO;
+		}
+		if ( letra == '#'){
+			matriz = arcoiris;
+		}
+		if ( letra == '@'){
+			matriz = mosaico;
+		}
+
+	for(int x = 0; x < tamanho_matriz; x++){
+		for(int y = 0; y < tamanho_matriz; y++){
+			if(matriz[x][y] == 'R'){
+				atribuir_cor_ao_led(matrizint[x][y],1,0,0);				
+			}
+			if(matriz[x][y] == 'G'){
+				atribuir_cor_ao_led(matrizint[x][y],0,1,0);
+			}
+			if(matriz[x][y] == 'B'){
+				atribuir_cor_ao_led(matrizint[x][y],0,0,1);
+			}  
+			if(matriz[x][y] == 'Y'){
+				atribuir_cor_ao_led(matrizint[x][y],1,1,0);
+			}  
+			if(matriz[x][y] == 'P'){
+				atribuir_cor_ao_led(matrizint[x][y],1,0,1);
+			}  
+			if(matriz[x][y] == 'C'){
+				atribuir_cor_ao_led(matrizint[x][y],0,1,1);
+			}  
+			if(matriz[x][y] == 'W'){
+				atribuir_cor_ao_led(matrizint[x][y],1,1,1);			}  
+
+			if(matriz[x][y] == '*'){
+				atribuir_cor_ao_led(matrizint[x][y],0,0,0);
+			} 
+		}			
+	}
+}
+
+	
+void colorir(char cor){
+
 }
 
 // Escreve os dados do buffer nos LEDs.
