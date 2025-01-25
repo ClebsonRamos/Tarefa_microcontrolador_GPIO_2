c6007248624b7ca5256ad10048a5fbe2da0ceca2
joão Lucas Fonseca Chagas <jlfc1463@gmail.com>
2025-01-25 19:32:26 -0300

    adicionado função para frequencia do buzzer'

--- Tarefa_microcontrolador_GPIO_2.c
+++ Tarefa_microcontrolador_GPIO_2.c
@@ -2,7 +2,8 @@
 #include "pico/stdlib.h"
 #include "hardware/pio.h"
 #include "hardware/clocks.h"
-#include "hardware/pwm.h"
+#include "include/frames.c"
+
 
 // Biblioteca gerada pelo arquivo .pio durante compilação.
 #include "ws2818b.pio.h"
@@ -10,7 +11,7 @@
 // Definição do número de LEDs e pino.
 #define CONTADOR_LED 25
 #define PINO_MATRIZ_LED 7
-//#define PINO_PWM_BRILHO 16 // a porta não esta certa, escolhi essa com o proposito de teste caso necessario troque
+// #define PINO_BUZZER não sei a porta
 // Definição de pixel GRB
 struct pixel_t {
 	uint8_t G, R, B; // Três valores de 8-bits compõem um pixel.
@@ -26,100 +27,53 @@ uint variavel_maquina_de_estado;
 
 //-----PROTÓTIPOS-----
 void inicializacao_maquina_pio(uint pino);
-void atribuir_cor_ao_led(const uint indice, const uint8_t r, const uint8_t g, const uint8_t b);
+void atribuir_cor_ao_led(const uint indice, const uint8_t r, const uint8_t g, const uint8_t b, uint8_t intensidade);
 void limpar_o_buffer();
 void escrever_no_buffer();
 void desenho(char letra);
-void inicializar_pwm(uint gpio,float clk_div ,uint16_t freq_pwm);
+void beep(int frequency)
+
+
 // ------MATRIZ-----
 
 int tamanho_matriz = 5;
 
 uint matrizint[5][5] = {
     {24, 23, 22, 21, 20},
-		{15, 16, 17, 18, 19},
-		{14, 13, 12, 11, 10},
-		{5, 6, 7, 8, 9},
-		{4, 3, 2, 1, 0}
+    {15, 16, 17, 18, 19},
+    {14, 13, 12, 11, 10},
+    {5, 6, 7, 8, 9},
+    {4, 3, 2, 1, 0}
 };
+void animar(int x, int y){
+	char temp;
+	temp = boneco[x][y];
+	boneco[x][y] = '*';
+	x = x + 2;
+	boneco[x][y] = temp;
 
- char matriz_B[5][5]={
-  {'*','B','B','*','*'},
-  {'*','B','*','B','*'},
-  {'*','B','B','*','*'},
-  {'*','B','*','B','*'},
-  {'*','B','B','*','*'}
- };
-
-char matriz_A[5][5]={
-  {'*','*','R','*','*'},
-  {'*','R','*','R','*'},
-  {'*','R','R','R','*'},
-  {'*','R','*','R','*'},
-  {'*','R','*','R','*'}
-};  
-char matriz_C[5][5]={
-  {'*','*','G','*','*'},
-  {'*','G','*','G','*'},
-  {'*','G','*','*','*'},
-  {'*','G','*','G','*'},
-  {'*','*','G','*','*'}
-};  
-char matriz_MARIO[5][5]={
-  {'R','R','R','R','R'},
-  {'R','P','P','P','P'},
-  {'R','B','R','R','B'},
-  {'B','B','B','B','B'},
-  {'Y','Y','*','Y','Y'}
-};  
-
-char arcoiris[5][5]={
-	{'R','R','Y','Y','Y'},
-  {'P','R','R','Y','G'},
-  {'P','P','W','G','G'},
-  {'P','B','C','C','G'},
-  {'B','B','B','C','C'}
-};
-char mosaico[5][5]={
-{ '*', '*', 'R', '*', '*' },
-{ '*', 'R', '*', 'B', '*' },
-{ '*', 'G', '*', 'Y', '*' },
-{ '*', 'B', '*', 'G', '*' },
-{ '*', '*', 'C', '*', '*' }
-};
+}
 
 
-void animacao_seta_para_direita(void);
-void animacao_seta_para_esquerda(void);
-void animacao_boneco(void);
-void animacao_coracao(void);
-void animacao_diagonal(void);
-void animacao_horizontal(void);
-
+uint8_t intensidade = 255; 
 //-----FUNÇÃO PRINCIPAL-----
 int main(void){
-
 	// Inicializa matriz de LEDs NeoPixel.
 	inicializacao_maquina_pio(PINO_MATRIZ_LED);
-    // aparentemente a placa de led não combina com pwm diretamento mais
-    inicializar_pwm(PINO_MATRIZ_LED , 4.0f ,4085);
-	sleep_ms(5000);
-	limpar_o_buffer();
-	desenho('#');
-	sleep_ms(3000);
-	desenho('A');
-	sleep_ms(3000);
-	desenho('B');
+	// inicializacao_maquina_pio(PINO_BUZZER)
+	 
+   limpar_o_buffer();	
+	 desenho('A');
+	 escrever_no_buffer();
+	 sleep_ms(3000);
 
-	
-	
-		
-	escrever_no_buffer(); // Escreve os dados nos LEDs.
+	 intensidade = 100;		
+   limpar_o_buffer();	
+	 desenho('A');
+	 escrever_no_buffer();
 
-	// A mágica acontece aqui :)
-	while (true){
-		
-	}
+
+
 	return 0;
 }
 
@@ -148,26 +102,29 @@ void inicializacao_maquina_pio(uint pino){
 		leds[i].B = 0;
 	}
 }
+
+
+
 // a função inicialar pwm precisa ser congigurada com o GPIO que vai ser ultilizado(led, ou buzzer) antes
 // frequencia deles é diferente e depois será configurada para uso
-void inicializar_pwm(uint gpio,float clk_div ,uint16_t wrap){
-	gpio_set_function(gpio,GPIO_FUNC_PWM);
-	uint slice_num = pwm_gpio_to_slice_num(gpio);
-	pwm_set_clkdiv(slice_num,clk_div);
-	pwm_set_wrap(slice_num,wrap);
-	pwm_set_enabled(slice_num,true);
-}
+
 // Atribui uma cor RGB a um LED.
-void atribuir_cor_ao_led(const uint indice, const uint8_t r, const uint8_t g, const uint8_t b){
-	leds[indice].R = r;
-	leds[indice].G = g;
-	leds[indice].B = b;
+void atribuir_cor_ao_led(const uint indice, const uint8_t r, const uint8_t g, const uint8_t b, uint8_t intensidade) {
+    
+    if (intensidade > 255) intensidade = 255;
+    if (intensidade < 0) intensidade = 0;
+
+    // Ajusta os valores de RGB conforme a intensidade escolhida
+    leds[indice].R = (r * intensidade) / 255;
+    leds[indice].G = (g * intensidade) / 255;
+    leds[indice].B = (b * intensidade) / 255;
 }
+
 
 // Limpa o buffer de pixels.
 void limpar_o_buffer(){	
 	for (uint i = 0; i < CONTADOR_LED; ++i){
-	atribuir_cor_ao_led(i,0,0,0);
+	atribuir_cor_ao_led(i,0,0,0, 255);
 	}			
 }
 
@@ -191,32 +148,40 @@ void  desenho(char letra){
 		if ( letra == '@'){
 			matriz = mosaico;
 		}
+		if ( letra == '1'){
+			matriz = matriz_1;
+		}
+		if (letra == '8')
+		{
+			matriz = boneco;
+		}
+		
 
 	for(int x = 0; x < tamanho_matriz; x++){
 		for(int y = 0; y < tamanho_matriz; y++){
 			if(matriz[x][y] == 'R'){
-				atribuir_cor_ao_led(matrizint[x][y],1,0,0);				
+				atribuir_cor_ao_led(matrizint[x][y],255,0,0, intensidade);				
 			}
 			if(matriz[x][y] == 'G'){
-				atribuir_cor_ao_led(matrizint[x][y],0,1,0);
+				atribuir_cor_ao_led(matrizint[x][y],0,1,0, intensidade);
 			}
 			if(matriz[x][y] == 'B'){
-				atribuir_cor_ao_led(matrizint[x][y],0,0,1);
+				atribuir_cor_ao_led(matrizint[x][y],0,0,1, intensidade);
 			}  
 			if(matriz[x][y] == 'Y'){
-				atribuir_cor_ao_led(matrizint[x][y],1,1,0);
+				atribuir_cor_ao_led(matrizint[x][y],1,1,0, intensidade);
 			}  
 			if(matriz[x][y] == 'P'){
-				atribuir_cor_ao_led(matrizint[x][y],1,0,1);
+				atribuir_cor_ao_led(matrizint[x][y],1,0,1, intensidade);
 			}  
 			if(matriz[x][y] == 'C'){
-				atribuir_cor_ao_led(matrizint[x][y],0,1,1);
+				atribuir_cor_ao_led(matrizint[x][y],0,1,1, intensidade);
 			}  
 			if(matriz[x][y] == 'W'){
-				atribuir_cor_ao_led(matrizint[x][y],1,1,1);			}  
+				atribuir_cor_ao_led(matrizint[x][y],1,1,1, intensidade);			}  
 
 			if(matriz[x][y] == '*'){
-				atribuir_cor_ao_led(matrizint[x][y],0,0,0);
+				atribuir_cor_ao_led(matrizint[x][y],0,0,0, intensidade);
 			} 
 		}			
 	}
@@ -239,417 +204,15 @@ void escrever_no_buffer(){
 }
 
 // ADICIONAR AS NOVAS FUNÇÕES A PARTIR DAQUI E INCLUIR O PROTÓTIPO.
-void animacao_seta_para_direita(void){
-    uint vetor[CONTADOR_LED], i, j, frames = 10;
+void beep(int frequency) {
+    int period = 1000000 / frequency; // Período em microssegundos
+    int half_period = period / 2;
 
-    for(i = 0; i < CONTADOR_LED; i++)
-        vetor[i] = 0;
-    
-    for(i = 0; i < frames; i++){
-        switch(i){
-            case 1:
-                // Liga
-                vetor[14] = 1;
-                break;
-            case 2:
-                // Liga
-                vetor[5] = 1;
-                vetor[13] = 1;
-                vetor[15] = 1;
-                break;
-            case 3:
-                // Liga
-                vetor[4] = 1;
-                vetor[6] = 1;
-                vetor[12] = 1;
-                vetor[16] = 1;
-                vetor[24] = 1;
-                // Desliga
-                vetor[14] = 0;
-                break;
-            case 4:
-                // Liga
-                vetor[3] = 1;
-                vetor[7] = 1;
-                vetor[11] = 1;
-                vetor[17] = 1;
-                vetor[23] = 1;
-                // Desliga
-                vetor[5] = 0;
-                vetor[13] = 0;
-                vetor[15] = 0;
-                break;
-            case 5:
-                // Liga
-                vetor[2] = 1;
-                vetor[8] = 1;
-                vetor[10] = 1;
-                vetor[18] = 1;
-                vetor[22] = 1;
-                // Desliga
-                vetor[4] = 0;
-                vetor[6] = 0;
-                vetor[12] = 0;
-                vetor[16] = 0;
-                vetor[24] = 0;
-                break;
-            case 6:
-                // Liga
-                vetor[1] = 1;
-                vetor[9] = 1;
-                vetor[19] = 1;
-                vetor[21] = 1;
-                // Desliga
-                vetor[3] = 0;
-                vetor[7] = 0;
-                vetor[11] = 0;
-                vetor[17] = 0;
-                vetor[23] = 0;
-                break;
-            case 7:
-                // Liga
-                vetor[0] = 1;
-                vetor[20] = 1;
-                // Desliga
-                vetor[2] = 0;
-                vetor[8] = 0;
-                vetor[10] = 0;
-                vetor[18] = 0;
-                vetor[22] = 0;
-                break;
-            case 8:
-                // Desliga
-                vetor[1] = 0;
-                vetor[9] = 0;
-                vetor[19] = 0;
-                vetor[21] = 0;
-                break;
-            case 9:
-                // Desliga
-                vetor[0] = 0;
-                vetor[20] = 0;
-                break;
-        }
-        for(j = 0; j < CONTADOR_LED; j++){
-            if(vetor[j] == 0){
-                atribuir_cor_ao_led(j, 0, 0, 0);
-            }else{
-                atribuir_cor_ao_led(j, 0, 0, 1);
-            }
-        }
-        escrever_no_buffer();
-        sleep_ms(100);
-    }
-}
+    for (int i = 0; i < 10; i++) {  // Repete 10 vezes para o som
+        gpio_put(BUZZER_PIN, 1); // Ativa o buzzer
+        sleep_us(half_period); // Atraso de meio período
 
-void animacao_seta_para_esquerda(void){
-    uint vetor[CONTADOR_LED], i, j, frames = 10;
-
-    for(i = 0; i < CONTADOR_LED; i++)
-        vetor[i] = 0;
-    
-    for(i = 0; i < frames; i++){
-        switch(i){
-            case 1:
-                // Liga
-                vetor[10] = 1;
-                break;
-            case 2:
-                // Liga
-                vetor[9] = 1;
-                vetor[11] = 1;
-                vetor[19] = 1;
-                break;
-            case 3:
-                // Liga
-                vetor[0] = 1;
-                vetor[8] = 1;
-                vetor[12] = 1;
-                vetor[18] = 1;
-                vetor[20] = 1;
-                // Desliga
-                vetor[10] = 0;
-                break;
-            case 4:
-                // Liga
-                vetor[1] = 1;
-                vetor[7] = 1;
-                vetor[13] = 1;
-                vetor[17] = 1;
-                vetor[21] = 1;
-                // Desliga
-                vetor[9] = 0;
-                vetor[11] = 0;
-                vetor[19] = 0;
-                break;
-            case 5:
-                // Liga
-                vetor[2] = 1;
-                vetor[6] = 1;
-                vetor[14] = 1;
-                vetor[16] = 1;
-                vetor[22] = 1;
-                // Desliga
-                vetor[0] = 0;
-                vetor[8] = 0;
-                vetor[12] = 0;
-                vetor[18] = 0;
-                vetor[20] = 0;
-                break;
-            case 6:
-                // Liga
-                vetor[3] = 1;
-                vetor[5] = 1;
-                vetor[15] = 1;
-                vetor[23] = 1;
-                // Desliga
-                vetor[1] = 0;
-                vetor[7] = 0;
-                vetor[13] = 0;
-                vetor[17] = 0;
-                vetor[21] = 0;
-                break;
-            case 7:
-                // Liga
-                vetor[4] = 1;
-                vetor[24] = 1;
-                // Desliga
-                vetor[2] = 0;
-                vetor[6] = 0;
-                vetor[14] = 0;
-                vetor[16] = 0;
-                vetor[22] = 0;
-                break;
-            case 8:
-                // Desliga
-                vetor[3] = 0;
-                vetor[5] = 0;
-                vetor[15] = 0;
-                vetor[23] = 0;
-                break;
-            case 9:
-                // Desliga
-                vetor[4] = 0;
-                vetor[24] = 0;
-                break;
-        }
-        for(j = 0; j < CONTADOR_LED; j++){
-            if(vetor[j] == 0){
-                atribuir_cor_ao_led(j, 0, 0, 0);
-            }else{
-                atribuir_cor_ao_led(j, 1, 0, 0);
-            }
-        }
-        escrever_no_buffer();
-        sleep_ms(100);
-    }
-}
-
-void animacao_boneco(void){
-    int i, frames;
-    uint vetor[CONTADOR_LED] = {
-        0, 1, 0, 1, 0,
-        0, 0, 1, 0, 0,
-        1, 0, 1, 0, 1,
-        0, 1, 1, 1, 0,
-        0, 0, 1, 0, 0
-    };
-
-    for(frames = 0; frames < 9; frames++){
-        // Atualiza os LEDs
-        switch(frames){
-            case 1:
-                vetor[10] = 0;
-                vetor[19] = 1;
-                break;
-            case 2:
-                vetor[19] = 0;
-                vetor[20] = 1;
-                break;
-            case 3:
-                vetor[19] = 1;
-                vetor[20] = 0;
-                break;
-            case 4:
-                vetor[19] = 0;
-                vetor[10] = 1;
-                break;
-            case 5:
-                vetor[14] = 0;
-                vetor[15] = 1;
-                break;
-            case 6:
-                vetor[15] = 0;
-                vetor[24] = 1;
-                break;
-            case 7:
-                vetor[24] = 0;
-                vetor[15] = 1;
-                break;
-            case 8:
-                vetor[15] = 0;
-                vetor[14] = 1;
-                break;
-        }
-        for(i = 0; i < CONTADOR_LED; i++){
-            if(vetor[i] == 1){
-                atribuir_cor_ao_led(i, 0, 0, 1);
-            } else{
-                atribuir_cor_ao_led(i, 0, 0, 0);
-            }
-        }
-        escrever_no_buffer();
-        sleep_ms(500);
-    }
-}
-
-void animacao_coracao(void){
-    int i, frames;
-    uint vetor[CONTADOR_LED] = {
-        0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0
-    };
-
-    for(frames = 0; frames < 6; frames++){
-        // Atualiza os LEDs
-        switch(frames){
-            case 1:
-                vetor[12] = 1;
-                break;
-            case 2:
-                vetor[12] = 2;
-                vetor[7] = 1;
-                vetor[11] = 1;
-                vetor[13] = 1;
-                vetor[17] = 1;
-                break;
-            case 3:
-                vetor[12] = 3;  
-                vetor[7] = 2;
-                vetor[11] = 2;
-                vetor[13] = 2;
-                vetor[17] = 2;
-                vetor[2] = 1;
-                vetor[6] = 1;
-                vetor[8] = 1;
-                vetor[10] = 1;
-                vetor[14] = 1;
-                vetor[16] = 1;
-                vetor[18] = 1;
-                vetor[22] = 1;
-                break;
-            case 4:
-                vetor[15] = 1;
-                vetor[19] = 1;
-                vetor[21] = 1;
-                vetor[23] = 1;
-                vetor[16] = 2;
-                vetor[18] = 2;
-                break;
-            case 5:
-                vetor[22] = 0;
-                vetor[17] = 1;
-                break;
-        }
-        for(i = 0; i < CONTADOR_LED; i++){
-            switch(vetor[i]){
-                case 0:
-                    atribuir_cor_ao_led(i, 0, 0, 0);
-                    break;
-                case 1:
-                    atribuir_cor_ao_led(i, 1, 0, 0);
-                    break;
-                case 2:
-                    atribuir_cor_ao_led(i, 0, 1, 0);
-                    break;
-                case 3:
-                    atribuir_cor_ao_led(i, 0, 0, 1);
-                    break;
-            }
-        }
-        escrever_no_buffer();
-        sleep_ms(500);
-    }
-}
-
-void animacao_diagonal(void){
-    int i, frames, alternador = 0;
-    uint vetor[CONTADOR_LED] = {
-        0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0
-    };
-
-    for(frames = 0; frames < 10; frames++){
-        for(i = 0; i < CONTADOR_LED; i++){
-            switch(alternador){
-                case 0:
-                    if(i % 2 == 0){
-                        vetor[i] = 1;
-                    }else{
-                        vetor[i] = 0;
-                    }
-                    break;
-                case 1:
-                    if(i % 2 == 1){
-                        vetor[i] = 1;
-                    }else{
-                        vetor[i] = 0;
-                    }
-                    break;
-            }
-        }
-        for(i = 0; i < CONTADOR_LED; i++){
-            if(vetor[i] == 1){
-                atribuir_cor_ao_led(i, 0, 0, 0);
-            }else{
-                atribuir_cor_ao_led(i, 0, 0, 1);
-            }
-        }
-        alternador++;
-        if(alternador == 2)
-            alternador = 0;
-        escrever_no_buffer();
-        sleep_ms(500);
-    }
-}
-
-void animacao_horizontal(void){
-    int i, j, frames, alternador = 0;
-    uint vetor[CONTADOR_LED] = {
-        0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0
-    };
-
-    for(frames = 0; frames < 10; frames++){
-        for(i = 0; i < 5; i++){
-            for(j = 0; j < 5; j++){
-                if(i % 2 == alternador){
-                    vetor[i * 5 + j] = 1;
-                }else{
-                    vetor[i * 5 + j] = 0;
-                }
-            }
-        }
-        alternador++;
-        if(alternador == 2)
-            alternador = 0;
-        for(i = 0; i < CONTADOR_LED; i++){
-            if(vetor[i] == 0){
-                atribuir_cor_ao_led(i, 1, 0, 1);
-            }else{
-                atribuir_cor_ao_led(i, 1, 1, 0);
-            }
-        }
-        escrever_no_buffer();
-        sleep_ms(500);
+        gpio_put(BUZZER_PIN, 0); // Desativa o buzzer
+        sleep_us(half_period); // Atraso de meio período
     }
 }
