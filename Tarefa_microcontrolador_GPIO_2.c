a66b3f18b9f695a6f91a591bb81db526ff3902a4
machadof90 <machadofilho.ti@gmail.com>
2025-01-26 00:20:17 -0300

    Remove matriz '1' comentada, ajusta a intensidade da matriz de LEDs e simplifica a função principal.

--- Tarefa_microcontrolador_GPIO_2.c
+++ Tarefa_microcontrolador_GPIO_2.c
@@ -43,68 +43,53 @@ uint matrizint[5][5] = {
 		{15, 16, 17, 18, 19},
 		{14, 13, 12, 11, 10},
 		{5, 6, 7, 8, 9},
-		{4, 3, 2, 1, 0}};
-
+		{4, 3, 2, 1, 0}
+		};
 
 uint8_t intensidade = 255;
+
+
 //-----FUNÇÃO PRINCIPAL-----
 int main(void)
 {
 	// Inicializa matriz de LEDs NeoPixel.
 	inicializacao_maquina_pio(PINO_MATRIZ_LED);
-	intensidade = 170;
+	intensidade = 40;
 	int tempo = 1000;
 	
-		limpar_o_buffer();
-		desenho('0');
-		escrever_no_buffer();
-	
-	/*limpar_o_buffer();
-	desenho('2');
-	escrever_no_buffer();
-	sleep_ms(tempo);
 
-	limpar_o_buffer();
-	desenho('2');
-	escrever_no_buffer();
-	sleep_ms(tempo);
-
-	limpar_o_buffer();
-	desenho('3');
-	escrever_no_buffer();
-	sleep_ms(tempo);
-
-	while(true){		
-		limpar_o_buffer();
+		
+		
 		desenho('4');
 		escrever_no_buffer();
 		sleep_ms(tempo);
 		
-		limpar_o_buffer();
+		
 		desenho('5');
 		escrever_no_buffer();
 		sleep_ms(tempo);
 		
-		limpar_o_buffer();
+	
 		desenho('6');
 		escrever_no_buffer();
 		sleep_ms(tempo);
 		
-		limpar_o_buffer();
+		
 		desenho('7');
 		escrever_no_buffer();
 		sleep_ms(tempo);
 		
-		limpar_o_buffer();
+		
 		desenho('8');
 		escrever_no_buffer();
 		sleep_ms(tempo);
 		
-		limpar_o_buffer();
+
 		desenho('9');
 		escrever_no_buffer();
 		sleep_ms(tempo);		
-	}*/
+		limpar_o_buffer;
+	
 	return 0;
 }
 
@@ -139,14 +124,7 @@ void inicializacao_maquina_pio(uint pino)
 
 // a função inicialar pwm precisa ser congigurada com o GPIO que vai ser ultilizado(led, ou buzzer) antes
 // frequencia deles é diferente e depois será configurada para uso
-void inicializar_pwm(uint gpio, float clk_div, uint16_t wrap)
-{
-	gpio_set_function(gpio, GPIO_FUNC_PWM);
-	uint slice_num = pwm_gpio_to_slice_num(gpio);
-	pwm_set_clkdiv(slice_num, clk_div);
-	pwm_set_wrap(slice_num, wrap);
-	pwm_set_enabled(slice_num, true);
-}
+
 
 // Atribui uma cor RGB a um LED.
 void atribuir_cor_ao_led(const uint indice, const uint8_t r, const uint8_t g, const uint8_t b, uint8_t intensidade)
@@ -178,7 +156,7 @@ void desenho(char letra)
 	
 	if (letra == '1')
 	{
-		//matriz = matriz_1;
+		matriz = matriz_1;
 	}
 	if (letra == '2')
 	{
@@ -247,7 +225,6 @@ void desenho(char letra)
 			{
 				atribuir_cor_ao_led(matrizint[x][y], 255, 255, 255, intensidade);
 			}
-
 			if (matriz[x][y] == '*')
 			{
 				atribuir_cor_ao_led(matrizint[x][y], 0, 0, 0, intensidade);
@@ -256,9 +233,6 @@ void desenho(char letra)
 	}
 }
 
-void colorir(char cor)
-{
-}
 
 // Escreve os dados do buffer nos LEDs.
 void escrever_no_buffer()
